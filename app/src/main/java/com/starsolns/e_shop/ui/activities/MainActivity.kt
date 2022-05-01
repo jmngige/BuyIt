4e7c9dcd0fb22ead7cc5609e4d46335155e86ff9
jmngige <jamesmngige@gmail.com>
2022-05-01 13:07:03 +0300

    Initial commit

--- app/src/main/java/com/starsolns/e_shop/ui/activities/MainActivity.kt
+++ app/src/main/java/com/starsolns/e_shop/ui/activities/MainActivity.kt
@@ -2,11 +2,21 @@ package com.starsolns.e_shop.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.starsolns.e_shop.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment
+        navController = navHost.findNavController()
     }
 }
--- app/src/main/java/com/starsolns/e_shop/ui/fragments/AccessFragment.kt
+++ app/src/main/java/com/starsolns/e_shop/ui/fragments/AccessFragment.kt
@@ -5,14 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
 import com.starsolns.e_shop.R
+import com.starsolns.e_shop.databinding.FragmentAccessBinding
+import com.starsolns.e_shop.ui.adapter.PagerAdapter
+
 class AccessFragment : Fragment() {
+
+    private var _binding: FragmentAccessBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_access, container, false)
+        _binding = FragmentAccessBinding.inflate(layoutInflater, container, false)
+
+        val fragments = ArrayList<Fragment>()
+        fragments.add(LoginFragment())
+        fragments.add(RegisterFragment())
+
+        val titles = ArrayList<String>()
+        titles.add("Login")
+        titles.add("Register")
+
+        var pagerAdapter = PagerAdapter(fragments, requireActivity())
+        binding.accessViewpager.apply {
+            adapter = pagerAdapter
+        }
+
+        TabLayoutMediator(binding.accessTabLayout, binding.accessViewpager){tab, position->
+            tab.text = titles[position]
+        }.attach()
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
