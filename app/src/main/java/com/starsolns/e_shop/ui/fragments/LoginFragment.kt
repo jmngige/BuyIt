b7baf09040c6dcaf52c155b8eb2538f7129b16a6
jmngige <jamesmngige@gmail.com>
2022-05-01 17:51:47 +0300

    login register ui design

--- app/src/main/java/com/starsolns/e_shop/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/starsolns/e_shop/ui/fragments/LoginFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.starsolns.e_shop.R
+import com.starsolns.e_shop.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
