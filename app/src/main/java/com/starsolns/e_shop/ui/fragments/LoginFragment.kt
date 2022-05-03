fa87fdaabf3ad6ac4e57355c62b8a587d991f728
jmngige <jamesmngige@gmail.com>
2022-05-03 14:54:45 +0300

    Register Login User

--- app/src/main/java/com/starsolns/e_shop/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/starsolns/e_shop/ui/fragments/LoginFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBindings
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.starsolns.e_shop.R
 import com.starsolns.e_shop.databinding.FragmentLoginBinding
 import com.starsolns.e_shop.util.ProgressButton
@@ -20,12 +24,16 @@ class LoginFragment : Fragment() {
     private lateinit var buttonView: View
     private lateinit var dialog: ProgressButton
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+
+        auth = Firebase.auth
 
         buttonView = binding.loginButton.loginRegisterAccessButton
 
@@ -44,22 +52,41 @@ class LoginFragment : Fragment() {
         val password = binding.loginPassword.text.toString().trim()
 
         if(email.isEmpty()){
-            binding.loginEmail.error = "Email Required"
-            binding.loginEmail.requestFocus()
+            binding.txtInputLoginEmail.helperText = "Email Required"
             return
+        }else{
+            binding.txtInputLoginEmail.helperText = null
         }
         if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            binding.loginEmail.error = "Enter a valid email "
-            binding.loginEmail.requestFocus()
+            binding.txtInputLoginEmail.helperText = "Enter a valid Email"
             return
+        }
+        else {
+            binding.txtInputLoginEmail.helperText = null
         }
         if(password.isEmpty()){
-            binding.loginPassword.error = "Password Required"
-            binding.loginPassword.requestFocus()
+            binding.txtInputLoginPassword.helperText = "Password Required"
             return
+        }else {
+            binding.txtInputLoginPassword.helperText = null
         }
 
+        loginUser(email, password)
 
+    }
+
+    private fun loginUser(email: String, password: String) {
+        dialog.showProgressBar()
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task->
+            if(task.isSuccessful){
+                dialog.dismissProgressBar()
+                Toast.makeText(requireContext(), "viola you're logged in", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                dialog.dismissProgressBar()
+                Toast.makeText(requireContext(), task.exception!!.message.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     override fun onDestroyView() {
