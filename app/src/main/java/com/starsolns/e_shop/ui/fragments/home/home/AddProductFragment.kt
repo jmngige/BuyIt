2a1950d964b870cb78574975b434fb954c58a615
jmngige <jamesmngige@gmail.com>
2022-05-18 18:19:55 +0300

    Add Products UI Design Impl commit

---
+++ app/src/main/java/com/starsolns/e_shop/ui/fragments/home/home/AddProductFragment.kt
@@ -0,0 +1,34 @@
+package com.starsolns.e_shop.ui.fragments.home.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.starsolns.e_shop.R
+import com.starsolns.e_shop.databinding.FragmentAddProductBinding
+import com.starsolns.e_shop.databinding.FragmentHomeBinding
+
+class AddProductFragment : Fragment() {
+
+    private var _binding: FragmentAddProductBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentAddProductBinding.inflate(layoutInflater, container, false)
+
+
+
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
